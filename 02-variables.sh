75bf628627c79b36ee7fe1adc9230da44dc85ffb
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-04 22:47:42 +0530

    added conversation

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@ PERSON1=prasad
 PERSON2=chandra
 
 echo "$PERSON1: Hi $PERSON2"
-echo "$PERSON2: Hello $PERSON1
+echo "$PERSON2: Hello $PERSON1"
 echo "$PERSON1: How are u doing?"
 echo "$PERSON2: I.m good, How about u?"

b3f7a1a5fb475867ce4930a41f89a54866ba680a
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-05 19:42:26 +0530

    shell script

--- 09-script-install.sh
+++ 09-script-install.sh
@@ -4,5 +4,5 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:: User must have sudo access to execute the script"
+    echo "ERROR:: User must have sudo access to execute the scripts"
 if

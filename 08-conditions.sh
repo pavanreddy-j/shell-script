7a8f25dad79088a3b372c6fbd5f983dd86a40e6a
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-05 17:59:29 +0530

    shell script

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,9 +3,9 @@
 NUMBER=$1
 # -gt, -et, -eq, -ne, -ge, -le 
 
-if [  $NUMBER -gt 100 ]
+if [  $NUMBER -gt 200 ]
 then
-echo "given number is greater than 100"
+echo "given number is greater than 200"
 else
-echo "given number is less than 100 or equal to 100"
+echo "given number is less than 200 or equal to 200"
 fi

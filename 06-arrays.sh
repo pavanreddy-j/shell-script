f3c57570f8a574e8ff80033d2eaea3ea1e81aadd
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-05 16:01:26 +0530

    shell script

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -3,7 +3,7 @@
 Movies=("rrr" "monark" "munna") 
 
 echo "First movie: ${Movies[0]}"
-echo "First movie: ${Movies[2]}"
-echo "First movie: ${Movies[1]}"
+echo "Second movie: ${Movies[2]}"
+echo "third movie: ${Movies[1]}"
 
 echo "ALL movies: ${Movies[@]}"

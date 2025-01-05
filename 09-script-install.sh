2610631f7999e6f25445cfce0ed82694a60f7126
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-05 19:54:52 +0530

    shell script

--- 09-script-install.sh
+++ 09-script-install.sh
@@ -5,4 +5,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute the scripts"
+    exit 1 # other than o
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing mysql ...FAILURE"
+    exit 1
+else
+    echo "Insatlling mysql ... SUCCESS"
 fi

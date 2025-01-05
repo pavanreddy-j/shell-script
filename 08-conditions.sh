83a0769bd66330f921e06acd14c768a08b842dcd
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-05 17:45:53 +0530

    shell script

---
+++ 08-conditions.sh
@@ -0,0 +1,10 @@
+#1/bib/bash
+
+NUMBER=$1
+# -gt, -et, -eq, -ne, -ge, -le 
+
+if [  $NUMBER -gt 100]
+then
+echo "given number is greater than 100"
+else
+echo "given number is less than 100 or equal to 100"

9b3b0fe6156e585c2cab8106a4eeef4cc8242c9e
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-11 09:56:31 +0530

    expense-shell

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -6,7 +6,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP= $(date +%Y-%m-%d-%H_%M_%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -31,16 +33,6 @@ CHECK_ROOT(){
 
 echo "Scripts started executing at : $TIMSTAMP" &>>$LOG_FILE_NAME
 
-CHECK_ROOT
-
-for package in $@
-do
-    dnf list installed $package &>>$LOG_FILE_NAME
-    if [ $? -ne 0 ]
-    then
-        dnf install $package -y &>>$LOG_FILE_NAME
-        VALIDATE $? "Installing $Package"
-    else
-        echo -e "$pacakge is already installed $Y ... INSTALLED $N"
-    fi
-done
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*logs" -mtime +14)
+    echo "Files to be
+     deleted: $FILES_TO_DELETE"

351d1690190d120433f3ce32a7d96fe78249e07f
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-11 12:02:49 +0530

    expense-shell

---
+++ 16-delete-logs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP= $(date +%Y-%m-%d-%H_%M_%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$1  ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e " $2... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute the scripts"
+        exit 1 # other than o
+    fi
+}
+
+echo "Scripts started executing at : $TIMSTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*logs" -mtime +14)
+    echo "The Files to be
+     deleted: $FILES_TO_DELETE"

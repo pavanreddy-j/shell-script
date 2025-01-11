43491f37d0b8ea46b0b2b062e054b9419ef68a32
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-11 21:00:11 +0530

    expense-shell

--- 16-delete-logs.sh
+++ 16-delete-logs.sh
@@ -36,7 +36,10 @@ echo "Scripts started executing at : $TIMSTAMP" &>>$LOG_FILE_NAME
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*logs" -mtime +14)
     echo "The Files to be deleted: $FILES_TO_DELETE"
 
-while read -r file 
+while read -r filepath # filepath is variable name, we can give any name
+
 do 
-echo "deleting file: $file"
+    echo "deleting file: $filepath" &>>$LOG_FILE_NAME
+    rm -rf $filepath
+    echo "deleting file: $filepath"
 done
--- 18-backup.sh
+++ 18-backup.sh
@@ -10,7 +10,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #if user is not providing number of days, we are taking 14 as default
 
-LOGS_FOLDER="/app-logs"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H_%M_%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -58,6 +58,19 @@ then
     echo "files are: $FILES"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMSTAMP.zip"
     find $SOURCE_DIR -name  "*.log" -mtime +$DAYS |zip -@ "$ZIP_FILE"
+    if [ -f "$ZIP_FILE" ]
+    then
+        echo -e " Successfully Zip file is created for older than $DAYS"
+        while read -r filepath # filepath is variable name, we can give any name
+            do 
+                echo "deleting file: $filepath" &>>$LOG_FILE_NAME
+                rm -rf $filepath
+                echo "deleting file: $filepath"
+            done
+    else
+        echo -e "$R Error:: $N Failed to create ZIP file"
+        exit 1
+    fi 
 else
     echo "No files found orlder than $DAYS"
 fi 
